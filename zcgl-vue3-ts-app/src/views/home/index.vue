814c41b7d26624f8ca0c56fb2331f18522b74bff
cgd_mac <349008059@qq.com>
2022-03-03 15:43:40 +0800

    feat: zcgl-pc初始化

---
+++ zcgl-vue3-ts-app/src/views/home/index.vue
@@ -0,0 +1,27 @@
+<!-- 作者：cgd  邮箱：349008059@qq.com  时间：2022年03月03日 11:11:56  -->
+<template>
+  <div>home.vue</div>
+</template>
+
+<script lang="ts">
+import { ref } from "@vue/reactivity";
+import { onMounted } from '@vue/runtime-core';
+interface LoginInterface {
+  username: string;
+  password: string;
+}
+
+export default {
+  props: {},
+  setup(): void {
+    let loginForm = ref<LoginInterface>({
+      username: "",
+      password: "",
+    });
+    onMounted(()=>{
+      console.log("12", loginForm.value.username)
+    })
+  },
+};
+</script>
+<style lang="scss" scoped></style>
---
+++ zcgl-vue3-ts-app/src/views/login/index.vue
@@ -0,0 +1,12 @@
+<!-- 作者：cgd  邮箱：349008059@qq.com  时间：2022年03月03日 11:12:07  -->
+<template>
+  <div>login.vue</div>
+</template>
+
+<script>
+
+export default {
+  props: {},
+};
+</script>
+<style lang="scss" scoped></style>
