53848864722809c875566f6bc3543af2741f6dba
cgd_mac <349008059@qq.com>
2022-03-03 11:09:43 +0800

    feat: 初始化app项目

--- zcgl-java/src/main/java/com/cgd/ZcglApplication.java
+++ zcgl-java/src/main/java/com/cgd/ZcglApplication.java
@@ -2,10 +2,18 @@ package com.cgd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ZcglApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZcglApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String login(){
+        return "Hello World!";
     }
 }
